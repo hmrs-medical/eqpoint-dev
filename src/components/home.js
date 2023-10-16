2c93dac489240d5de7ddf6f8f2025cba3e32e0b0
mj1999 <mayank.joshi92@yahoo.com>
2023-10-16 13:02:25 +0530

    added slider component updates, and some style updates

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
       <section id="intro">
         <div id="first-display">
-          <div id="intro-collage">
+          <div className="intro-collage">
             <img
               style={{ borderRadius: "inherit" }}
               src={collage}
@@ -15,9 +15,8 @@ function Home() {
               width="100%"
               alt="medical equipments collage"
             />
-            {/* <Link style={{textDecoration:'none',color:'black'}} to='/products'><div>View-Products</div></Link> */}
           </div>
-          <div id="intro-about" className="subtext">
+          <div className="intro-about subtext">
             A well-diversified company that supplies quality products
             manufactured by reliable resources and professional medical service
             representatives. We have established a reputation of trust with our
@@ -26,14 +25,14 @@ function Home() {
           </div>
         </div>
         <div id="second-display">
-          <div id="intro-about" className="subtext">
+          <div className="intro-about subtext">
             A well-diversified company that supplies quality products
             manufactured by reliable resources and professional medical service
             representatives. We have established a reputation of trust with our
             customers in terms of excellent, efficient, and prompt supply of
             products.
           </div>
-          <div id="intro-collage">
+          <div className="intro-collage">
             <img
               style={{ borderRadius: "inherit" }}
               src={collage}
@@ -48,39 +47,50 @@ function Home() {
       <section id="about">
         <h1 className="section-heading">About us</h1>
         <div id="about-display">
-          <div className="subtext" >
-          Established in March 2005 as a distribution company with an aim of marketing and trading medical 
-          equipments, medical devices and disposables in most efficient manner.
-          Our main focus is to develop professional relations with the customers to understand
-          their need and offer solutions to fullfill those needs. We strive to provide best services with 
-          promt response.
+          <div className="subtext">
+            Established in March 2005 as a distribution company with an aim of
+            marketing and trading medical equipments, medical devices and
+            disposables in most efficient manner. Our main focus is to develop
+            professional relations with the customers to understand their need
+            and offer solutions to fullfill those needs. We strive to provide
+            best services with promt response.
           </div>
           <h2 className="subheading">Focus Areas</h2>
-          <ul className="subtext" style={{width:'70vw'}}>
-            <li>
-              Transfusion medicine
-            </li>
+          <ul className="subtext" style={{ width: "70vw" }}>
+            <li>Transfusion medicine</li>
             <li>Hematology with special focus on BMT and Thallesemia</li>
-            <li>Diagnostic oncology & research equipments & solutions designing blood bank as per regulatory requirement</li>
+            <li>
+              Diagnostic oncology & research equipments & solutions designing
+              blood bank as per regulatory requirement
+            </li>
             <li>Stand-alone laboratories & hospital based laboratories</li>
             <li>Turn key blood bank projects with best quality equipments</li>
           </ul>
           <h2 className="subheading">Team</h2>
           <div className="subtext">
-            A strong back up of team comprising of engineers, sales managers, application specialists, manager logistics and administrative manager
-            headed by management graduates with more than 2 decade of pharma & medical equipment industry experience.
+            A strong back up of team comprising of engineers, sales managers,
+            application specialists, manager logistics and administrative
+            manager headed by management graduates with more than 2 decade of
+            pharma & medical equipment industry experience.
           </div>
           <h2 className="subheading">Principal Companies</h2>
-          <ul className="subtext" style={{width:'70vw'}}>
-            <li>
-              TERUMO BCT  
-            </li>
+          <ul className="subtext" style={{ width: "70vw" }}>
+            <li>TERUMO BCT</li>
             <li>THERMO FISCHER SCIENTIFIC</li>
             <li>OPTICS TECHNOLOGIES Ltd.</li>
             <li>LISA LINE</li>
             <li>HEMOCUE</li>
           </ul>
-
+          <h2 className="subheading">Office</h2>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3499.430815303244!2d77.10946547614591!3d28.706668380708834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d0157b90bf289%3A0x86dc0d6eae2e7732!2sEquipment%20Point!5e0!3m2!1sen!2sin!4v1697293785094!5m2!1sen!2sin"
+            width="600"
+            height="450"
+            style={{ border: "0", borderRadius: "10px", marginLeft: "1vw" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </section>
       <section id="clientel">
@@ -88,7 +98,7 @@ function Home() {
           Building relationships based on trust
         </h1>
         <div id="clientel-display">
-            <Slider/>
+          <Slider numSlides={4} height={"30vh"} width={"30vw"} />
         </div>
       </section>
     </div>
--- src/components/slider.js
+++ src/components/slider.js
@@ -1,12 +1,49 @@
-import '../assets/css/slider.css'
-function Slider(){
-    return (
-    <div className="slider-display">
-        
-    </div>
-    );
+import { useEffect, useState } from "react";
+import "../assets/css/slider.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+function Slider(props) {
+  const [index, setIndex] = useState(1);
+  const numSlides = props.numSlides;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const isLast = index === numSlides;
+      setIndex(isLast ? 1 : index + 1);
+    }, 6000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [index, numSlides]);
 
-    
+  const next = () => {
+    const isLast = index === numSlides;
+    setIndex(isLast ? 1 : index + 1);
+  };
+  const prev = () => {
+    const isLast = index === 1;
+    setIndex(isLast ? numSlides - 1 : index - 1);
+  };
+
+  return (
+    <div
+      className="slider-display"
+      style={{ height: props.height, width: props.width }}
+    >
+      <div className="left-arrow arrow">
+        <FontAwesomeIcon icon={faChevronLeft} onClick={prev} />
+      </div>
+      <div className="right-arrow arrow">
+        <FontAwesomeIcon icon={faChevronRight} onClick={next} />
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(http://localhost:3000/img${index}.jpeg)`,
+        }}
+        className="slider-container"
+      ></div>
+    </div>
+  );
 }
 
 export default Slider;
