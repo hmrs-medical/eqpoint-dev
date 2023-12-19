9ab653f9f676c0d7b2990918096082268dc3eca4
mj1999 <mayank.joshi92@yahoo.com>
2023-12-19 16:08:03 +0530

    product-list category updates

--- src/components/config_flies/products.js
+++ src/components/config_flies/products.js
@@ -3,7 +3,6 @@ export const prodCategory = {
   laboratory: [
     "Clinical Labs & Immunodiagnostics",
     "Immunohematology",
-    "Donor Screening",
   ],
   research: ["Laboratory and Storage equipments", "Sterile Connecting Devices"],
   coldchain: ["Active and Passive cooling transportation", "Data loggers"],
